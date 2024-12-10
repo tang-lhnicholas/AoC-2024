901e3ccf77f4685c3afc547400b21501c7ac70f3
tang-lhnicholas <165698701+tang-lhnicholas@users.noreply.github.com>
2024-12-10 19:32:57 +0000

    Add files via upload

---
+++ day_09.py
@@ -0,0 +1,39 @@
+
+data = []
+
+with open('AoC 2024 Day 9 Input.txt') as file:
+    disk_map = file.read()
+
+ID = 0
+for i in range(len(disk_map)):
+    if not i%2: # For digits at even positions
+        for n in range(int(disk_map[i])): data.append(ID)
+        ID += 1
+    else:
+        for n in range(int(disk_map[i])): data.append('.')
+
+# Part One
+while not ''.join(map(str, data)).isnumeric():
+    if data[-1] == '.': data.pop(-1)
+    else:
+        data[data.index('.')] = data[-1]
+        data.pop(-1)
+
+checksum = 0
+for i in range(len(data)):
+    checksum += i * data[i]
+
+print(checksum)
+
+# Part Two
+while not ''.join(map(str, data)).isnumeric():
+    if data[-1] == '.': data.pop(-1)
+    else:
+        data[data.index('.')] = data[-1]
+        data.pop(-1)
+
+checksum = 0
+for i in range(len(data)):
+    checksum += i * data[i]
+
+print(checksum)
